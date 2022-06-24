93d0f053b69ec22ee0d138907190696f7cdc4a04
qswdeef <>
2022-06-24 16:45:45 +0800

    change

--- backend/src/main/java/uto/fungumi/backend/controller/UserRegisterController.java
+++ backend/src/main/java/uto/fungumi/backend/controller/UserRegisterController.java
@@ -1,9 +1,9 @@
 package uto.fungumi.backend.controller;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import uto.fungumi.backend.entity.User;
 import uto.fungumi.backend.model.BaseResult;
@@ -11,22 +11,22 @@ import uto.fungumi.backend.service.serviceImpl.UserServiceImpl;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
-public class UserController {
+@RequestMapping("/register")
+public class UserRegisterController {
     @Resource
     private UserServiceImpl userService;
 
 
-    @GetMapping("/register")
+    @GetMapping("/")
     public String regist(){
         return "register";
     }
 
 
     @ResponseBody
-    @PostMapping("/register/addUser")
+    @PostMapping("/addUser")
     public BaseResult<User> addUser(User user){
         BaseResult<User> baseResult = new BaseResult<>();
         if (userService.findByUsername(user.getUsername()) == null){
@@ -42,29 +42,5 @@ public class UserController {
         return baseResult;
     }
 
-
-    @GetMapping("/login")
-    public String login(){
-        return "login";
-    }
-
-
-    @ResponseBody
-    @PostMapping("/login/checkUsernameAndPassword")
-    public BaseResult<User> checkUsernameAndPassword(User user, HttpServletRequest request){
-        BaseResult<User> baseResult = new BaseResult<>();
-        User byUsernameAndPassword = userService.findByUsernameAndPassword(user.getUsername(), user.getPassword());
-        if (byUsernameAndPassword == null){
-            baseResult.setSuccess(false);
-            baseResult.setMessage("failed to login！");
-        }
-        else {
-            baseResult.setSuccess(true);
-            baseResult.setData(byUsernameAndPassword);
-            request.getSession().setAttribute("username",byUsernameAndPassword.getUsername());
-
-        }
-        return baseResult;
-    }
 
 }
