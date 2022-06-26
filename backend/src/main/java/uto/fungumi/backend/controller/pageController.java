703ef5c9039665c5134a846805c297768bd6e552
wizard75 <2856064573@qq.com>
2022-06-26 13:27:03 +0800

    忘记包BaseResult了，包上

--- backend/src/main/java/uto/fungumi/backend/controller/pageController.java
+++ backend/src/main/java/uto/fungumi/backend/controller/pageController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uto.fungumi.backend.dao.WorkDao;
 import uto.fungumi.backend.entity.Work;
+import uto.fungumi.backend.model.BaseResult;
 
 import javax.annotation.Resource;
 
@@ -18,32 +19,11 @@ public class pageController {
     private WorkDao workDao;
 
 
-    @GetMapping("/page/id")
-    public Object findAllById(@RequestParam(defaultValue = "0", value = "pageNo") Integer pageNo,
-                          @RequestParam(defaultValue = "10", value = "pageSize") Integer pageSize){
-            Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.Direction.ASC, "id");
-            Page<Work> pages = workDao.findAll(pageable);
-            // 当前页数
-            System.out.println("当前页数：" + pages.getNumber());
-            // 总页数
-            System.out.println("总页数：" + pages.getTotalPages());
-            // 查询出来的所有数据
-            System.out.println("查询出来的所有数据：" + pages.getContent());
-            // 是否有上一页
-            System.out.println("是否有上一页：" + pages.hasPrevious());
-            // 是否有下一页
-            System.out.println("是否有下一页：" + pages.hasNext());
-            // 当前页的上一页
-            System.out.println("当前页的上一页：" + (pages.hasPrevious() ? pages.previousPageable().getPageNumber() : pages.getNumber()));
-            // 当前页的下一页
-            System.out.println("当前页的下一页：" + (pages.hasNext() ? pages.nextPageable().getPageNumber() : pages.getNumber()));
-            return pages;
-
-    }
     @GetMapping("/page/rank")
-    public Object findAllByRank(@RequestParam(defaultValue = "0", value = "pageNo") Integer pageNo,
-                              @RequestParam(defaultValue = "10", value = "pageSize") Integer pageSize){
-        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.Direction.DESC, "score");
+    public BaseResult<Page<Work>> findAllById(@RequestParam(defaultValue = "0", value = "pageNo") Integer pageNo,
+                                                @RequestParam(defaultValue = "10", value = "pageSize") Integer pageSize){
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.Direction.DESC, "id");
+        BaseResult<Page<Work>> baseResult = new BaseResult<>();
         Page<Work> pages = workDao.findAll(pageable);
         // 当前页数
         System.out.println("当前页数：" + pages.getNumber());
@@ -59,7 +39,48 @@ public class pageController {
         System.out.println("当前页的上一页：" + (pages.hasPrevious() ? pages.previousPageable().getPageNumber() : pages.getNumber()));
         // 当前页的下一页
         System.out.println("当前页的下一页：" + (pages.hasNext() ? pages.nextPageable().getPageNumber() : pages.getNumber()));
-        return pages;
+        if(pages != null){
+            baseResult.setData(pages);
+            baseResult.setSuccess(true);
+            baseResult.setMessage("分页数据传输成功");
+        }else {
+            baseResult.setSuccess(false);
+            baseResult.setMessage("分页数据传输失败");
+        }
+
+        return baseResult;
+
+    }
+    @GetMapping("/page/rank")
+    public BaseResult<Page<Work>> findAllByRank(@RequestParam(defaultValue = "0", value = "pageNo") Integer pageNo,
+                              @RequestParam(defaultValue = "10", value = "pageSize") Integer pageSize){
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.Direction.DESC, "score");
+        BaseResult<Page<Work>> baseResult = new BaseResult<>();
+        Page<Work> pages = workDao.findAll(pageable);
+        // 当前页数
+        System.out.println("当前页数：" + pages.getNumber());
+        // 总页数
+        System.out.println("总页数：" + pages.getTotalPages());
+        // 查询出来的所有数据
+        System.out.println("查询出来的所有数据：" + pages.getContent());
+        // 是否有上一页
+        System.out.println("是否有上一页：" + pages.hasPrevious());
+        // 是否有下一页
+        System.out.println("是否有下一页：" + pages.hasNext());
+        // 当前页的上一页
+        System.out.println("当前页的上一页：" + (pages.hasPrevious() ? pages.previousPageable().getPageNumber() : pages.getNumber()));
+        // 当前页的下一页
+        System.out.println("当前页的下一页：" + (pages.hasNext() ? pages.nextPageable().getPageNumber() : pages.getNumber()));
+        if(pages != null){
+            baseResult.setData(pages);
+            baseResult.setSuccess(true);
+            baseResult.setMessage("分页数据传输成功");
+        }else {
+            baseResult.setSuccess(false);
+            baseResult.setMessage("分页数据传输失败");
+        }
+
+        return baseResult;
 
     }
 
