ee74fa2aa39ca0926c1c5b59e93604b8bc9e9d14
wizard75 <2856064573@qq.com>
2022-06-26 13:18:15 +0800

    分页框架写完了，看有什么功能加什么分页

--- backend/src/main/java/uto/fungumi/backend/dao/WorkDao.java
+++ backend/src/main/java/uto/fungumi/backend/dao/WorkDao.java
@@ -1,5 +1,7 @@
 package uto.fungumi.backend.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import uto.fungumi.backend.entity.Work;
@@ -9,4 +11,5 @@ import java.util.List;
 public interface WorkDao extends JpaRepository<Work,Integer> {
     @Query("select w from work w where w.category = ?1")
     List<Work> findAllByCategory(String category);
+    Page<Work> findAll(Pageable pageable);
 }
--- backend/src/main/java/uto/fungumi/backend/service/DisplayService.java
+++ backend/src/main/java/uto/fungumi/backend/service/DisplayService.java
@@ -1,7 +1,22 @@
 package uto.fungumi.backend.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uto.fungumi.backend.dao.WorkDao;
 import uto.fungumi.backend.entity.Work;
 
 import java.util.List;
-public interface DisplayService {
-    List<Work> selectByCategory(String category);
+@Service
+public class DisplayService{
+    @Autowired
+    WorkDao workDao;
+
+    public List<Work> selectByCategory(String category) {
+        List<Work> works = workDao.findAllByCategory(category);
+        if (works.size() > 6) {
+            for (int i = 6; i < works.size(); i++) {
+                works.remove(i);
+            }
+        }
+        return works;
+    }
 }
